fdfa131124c1b1867662ad252b1ddd402e40aa4b
Júlia Castro <julia.gabrielly2011@gmail.com>
2023-08-02 01:06:57 -0300

    feat: :sparkles: Criando as funções de deletar e cadastrar produtos
    Criando as funções de deletar e cadastrar produtos

--- server/functions.php
+++ server/functions.php
@@ -1,6 +1,18 @@
 <?php
-    include_once './server/db.php';  
+    include_once 'db.php';  
 
+    if(array_key_exists('requisicao', $_REQUEST)){
+        switch($_REQUEST['requisicao']) {
+            case 'cadastrar':
+                cadastrarUsuarios($_REQUEST);
+                header('Location: /index.php?cadastrado=true');
+                break;
+            case 'deletar':
+                deletarUsuario($_REQUEST['id']);
+                header('Location: /index.php?deletado=true');
+                break;
+        } 
+    }
     
     
     function listarUsuarios() {
@@ -15,7 +27,6 @@
         $arrayUsuarios = [];
 
         if ($dadosUsuarios->num_rows > 0) {
-            // output data of each row
             while($row = $dadosUsuarios->fetch_assoc()) {
               $arrayUsuarios[] = $row;
             }
@@ -24,3 +35,51 @@
         $conn->close();
         return $arrayUsuarios;
     };
+
+    function cadastrarUsuarios($dadosUsuario){
+        $todasPermissoes = '';
+        foreach($dadosUsuario['permissao'] as $permissao){
+            switch ($permissao){
+                case 'login':
+                    $todasPermissoes .= 'l';
+                    break;
+                case 'usuario_add':
+                    $todasPermissoes .= 'a';
+                    break;
+                case 'usuario_editar':
+                    $todasPermissoes .= 'e';
+                    break;
+                case 'usuario_deletar':
+                    $todasPermissoes .= 'd';
+                    break;
+            }
+        }
+
+        $dataAtual = date('Y-m-d h:m:s');
+        $uuid = uniqid();
+        $status = $_REQUEST['status'] == 'ativo' ? 1 : 0;
+
+        print_r($dataAtual);
+
+        $sql = <<< SQL
+            INSERT INTO usuario (`uuid`, `nome`, `cpf`, `email`, `senha`, `permissao`, `data_criacao`, `data_atualizacao`, `status`)
+        VALUES (
+            '{$uuid}', '{$dadosUsuario['nome']}', {$dadosUsuario['cpf']}, '{$dadosUsuario['email']}', '{$dadosUsuario['senha']}', 
+            '$todasPermissoes','{$dataAtual}', '{$dataAtual}', '{$status}'
+        )
+        SQL; 
+
+        global $conn;
+        $conn->query($sql);
+        $conn->close();
+    };
+
+    function deletarUsuario($idUsuario) {
+        $sql = <<<SQL
+            DELETE FROM usuario WHERE id = {$idUsuario};
+        SQL;
+
+        global $conn;
+        $conn->query($sql);
+        $conn->close();
+    }
